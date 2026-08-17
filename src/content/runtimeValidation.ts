9acc262fb791ae085a34015f3658b6fa6698b78e
tametheboardgame <fairbairn.dave@yahoo.co.uk>
2026-08-17 13:36:41 +0100

    WP0.3A validate combined runtime content

--- src/content/runtimeValidation.ts
+++ src/content/runtimeValidation.ts
@@ -1,6 +1,6 @@
 import { assertValidContentCatalog } from '../domain/validation.js';
-import { PROTOTYPE_CONTENT_CATALOG } from './prototypeCatalog.js';
+import { CONTENT_CATALOG } from './contentCatalog.js';
 
 export function validateRuntimeContent(): void {
-  assertValidContentCatalog(PROTOTYPE_CONTENT_CATALOG);
+  assertValidContentCatalog(CONTENT_CATALOG);
 }
