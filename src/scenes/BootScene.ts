3a7e19a4cb550b9dfad94dec7a6aea8dd5b08419
tametheboardgame <fairbairn.dave@yahoo.co.uk>
2026-08-18 07:14:38 +0100

    WP0.4C install visual remap at boot

--- src/scenes/BootScene.ts
+++ src/scenes/BootScene.ts
@@ -1,11 +1,13 @@
 import Phaser from 'phaser';
 import { PALETTE } from '../config.js';
 import { transitionTo } from '../ui/transitions.js';
+import { installPrototypeColourRemap } from '../ui/visualTheme.js';
 
 export class BootScene extends Phaser.Scene {
   constructor() { super('Boot'); }
 
   create(): void {
+    installPrototypeColourRemap();
     this.cameras.main.setBackgroundColor(PALETTE.paperDeep);
     transitionTo(this, 'Title', { duration: 0 });
   }
