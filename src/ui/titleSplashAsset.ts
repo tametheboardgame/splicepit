68e4f17a799c732ada34fc2039ac842997def839
tametheboardgame <fairbairn.dave@yahoo.co.uk>
2026-08-25 12:55:54 +0100

    Assemble happy splash title asset

---
+++ src/ui/titleSplashAsset.ts
@@ -0,0 +1,8 @@
+import { TITLE_SPLASH_PART_0 } from './titleSplashPart0.js';
+import { TITLE_SPLASH_PART_1 } from './titleSplashPart1.js';
+import { TITLE_SPLASH_PART_2 } from './titleSplashPart2.js';
+import { TITLE_SPLASH_PART_3 } from './titleSplashPart3.js';
+import { TITLE_SPLASH_PART_4 } from './titleSplashPart4.js';
+
+export const HAPPY_TITLE_SPLASH_DATA_URI =
+  `data:image/webp;base64,${TITLE_SPLASH_PART_0}${TITLE_SPLASH_PART_1}${TITLE_SPLASH_PART_2}${TITLE_SPLASH_PART_3}${TITLE_SPLASH_PART_4}`;
