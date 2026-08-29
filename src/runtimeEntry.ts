081c0b0e6685c6192d4f7a7b5cb258f3e7de75e8
tametheboardgame <fairbairn.dave@yahoo.co.uk>
2026-08-29 09:24:51 +0100

    YSP-0 add development renderer switch

---
+++ src/runtimeEntry.ts
@@ -0,0 +1,10 @@
+const query = new URLSearchParams(window.location.search);
+const yardRenderer = query.get('yardRenderer');
+
+if (yardRenderer === 'scene-image') {
+  const { startYardSceneImageSpike } = await import('./yardSceneImageSpike.js');
+  const started = await startYardSceneImageSpike();
+  if (!started) await import('./boot.js');
+} else {
+  await import('./boot.js');
+}
